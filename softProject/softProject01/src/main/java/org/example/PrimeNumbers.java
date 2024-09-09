5951a3d226f0ec6b77fbe074a32a722572d3c025
xieyangyuyue <1483618794@qq.com>
2024-09-09 20:48:39 +0800

    [add] 添加PrimeNumbers类 用于判断素数

---
+++ softProject/softProject01/src/main/java/org/example/PrimeNumbers.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 2; i <= 20000; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+                count++;
+                if (count % 5 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
