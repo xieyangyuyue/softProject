b288f69908505b739ea04ab83ff521628380fe8e
xieyangyuyue <1483618794@qq.com>
2024-09-10 12:31:03 +0800

    [add]修正测试代码并添加测试用例

--- softProjectTest01/src/main/java/org/example/Triangle.java
+++ softProjectTest01/src/main/java/org/example/Triangle.java
@@ -3,11 +3,6 @@ package org.example;
 public class Triangle {
 
    public String classify(int a, int b, int c) {
-      // 检查边长是否满足条件3
-      if (a < 1 || a > 100 || b < 1 || b > 100 || c < 1 || c > 100) {
-         return "输入错误";
-      }
-
       // 检查是否满足条件4（构成三角形的必要条件）
       if (!((a + b > c) && (a + c > b) && (b + c > a))) {
          return "非三角形";
